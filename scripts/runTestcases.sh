0b59b7f5efc0fce66fd7408488f31f0ca13838e0
narendr432 <89505680+narendr432@users.noreply.github.com>
2023-12-07 18:01:54 +0530

    Update runTestcases.sh

--- scripts/runTestcases.sh
+++ scripts/runTestcases.sh
@@ -2,7 +2,7 @@
 
 gradle_tasks="clean test"
 base_gradle_parameters="-Dcucumber.options=\"--tags @smoke\"" #${TEST_GROUP}
-echo $base_gradle_parameters
+echo "----------------> "$base_gradle_parameters
 gradle_parameters="$base_gradle_parameters"
 chmod +x ./gradlew # making executable
 echo "$gradle_tasks $gradle_parameters $env_url" | xargs ./gradlew
