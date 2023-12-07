0be3be33acb33cbaad8e34525b7d63abcdeaa328
narendr432 <89505680+narendr432@users.noreply.github.com>
2023-12-07 19:51:07 +0530

    Update LoginSteps.java

--- src/test/java/StepDefinition/LoginSteps.java
+++ src/test/java/StepDefinition/LoginSteps.java
@@ -11,43 +11,18 @@ import org.openqa.selenium.chrome.ChromeOptions;
 public class LoginSteps {
 
   public WebDriver driver=null;
- /*
-    @Given("user launch the chrome browser")
-    public void user_launch_the_chrome_browser() {
 
-        System.setProperty("web-driver.chrome.driver","C://Users//user//IdeaProjects//Guru99Bank//Drivers//chromedriver.exe");
-        driver= new ChromeDriver();
-    }
-
-    @When("user navigate to the login page")
-    public void user_navigate_to_the_login_page() {
-        driver.get("https://demo.guru99.com/V1/index.php");
-        driver.manage().window().maximize();
-    }
-
-    @Then("user enter the username and password")
-    public void user_enter_the_username_and_password() {
-        driver.findElement(By.name("uid")).sendKeys("mngr541220");
-        driver.findElement(By.name("password")).sendKeys("raduqyp");
-        driver.findElement(By.name("btnLogin")).click();
-
-    }
-
-    @And("user navigate to homepage")
-    public void user_navigate_to_homepage() {
-        driver.close();
-
-    }*/
 
     @Given("user is on login page")
     public void user_is_on_login_page() {
         System.setProperty("webdriver.chrome.driver","C:\\Users\\user\\IdeaProjects\\Guru99Bank\\Drivers\\chromedriver.exe");
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
-      options.setHeadless(false);
+        options.setHeadless(false);
         driver= new ChromeDriver(options);
         driver.manage().window().maximize();
         driver.get("https://demo.guru99.com/V1/index.php");
+        System.out.println("--------------------------");
 
     }
     @When("user enter the username and password")
@@ -62,6 +37,7 @@ public class LoginSteps {
     }
     @Then("user navigate to homepage")
     public void user_navigate_to_homepage() {
+        driver.close();
 
     }
 
