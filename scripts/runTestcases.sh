648a2622245bda546ed35abafbd1508c9a26e07c
narendr432 <89505680+narendr432@users.noreply.github.com>
2023-12-07 20:15:50 +0530

    Update runTestcases.sh

--- scripts/runTestcases.sh
+++ scripts/runTestcases.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+set -ex
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb
 gradle_tasks="clean test"
 base_gradle_parameters="\"-Dcucumber.options=--tags @smoke\"" #${TEST_GROUP}
 gradle_parameters="$base_gradle_parameters"
