663ce37daed981c96af0ce0e46aa103d4243646b
narendr432 <89505680+narendr432@users.noreply.github.com>
2023-12-07 19:57:01 +0530

    Update LoginSteps.java

--- src/test/java/StepDefinition/LoginSteps.java
+++ src/test/java/StepDefinition/LoginSteps.java
@@ -15,29 +15,29 @@ public class LoginSteps {
 
     @Given("user is on login page")
     public void user_is_on_login_page() {
-       /* System.setProperty("webdriver.chrome.driver","C:\\Users\\user\\IdeaProjects\\Guru99Bank\\Drivers\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\user\\IdeaProjects\\Guru99Bank\\Drivers\\chromedriver.exe");
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
-        options.setHeadless(false);
+        options.setHeadless(true);
         driver= new ChromeDriver(options);
         driver.manage().window().maximize();
-        driver.get("https://demo.guru99.com/V1/index.php");*/
+        driver.get("https://demo.guru99.com/V1/index.php");
         System.out.println("--------------------------");
 
     }
     @When("user enter the username and password")
     public void user_enter_the_username_and_password() {
-       // driver.findElement(By.name("uid")).sendKeys("mngr541220");
-       // driver.findElement(By.name("password")).sendKeys("raduqyp");
+        driver.findElement(By.name("uid")).sendKeys("mngr541220");
+        driver.findElement(By.name("password")).sendKeys("raduqyp");
 
     }
     @Then("click on login button")
     public void click_on_login_button() {
-      //  driver.findElement(By.name("btnLogin")).click();
+        driver.findElement(By.name("btnLogin")).click();
     }
     @Then("user navigate to homepage")
     public void user_navigate_to_homepage() {
-       // driver.close();
+        driver.close();
 
     }
 
